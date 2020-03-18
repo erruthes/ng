675d76cddba04b84213e59760f25d929e9869728
Eduardo Ruthes <eruthes@gmail.com>
2020-03-17 22:25:49 -0300

    criação do Modulo InputOutputProperty

---
+++ src/app/input-output-properties/input-output-properties.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputPropertyComponent } from './input-property/input-property.component';
+
+
+
+@NgModule({
+  declarations: [InputPropertyComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class InputOutputPropertiesModule { }
