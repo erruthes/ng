6d9c9feb6031a057749a6fb05f47ae4480dc8fb3
Eduardo Ruthes <eruthes@gmail.com>
2020-03-19 23:37:14 -0300

    Correções e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ngLoiane app is running!');
+    expect(page.getTitleText()).toEqual('ngLoiane');
   });
 
   afterEach(async () => {
