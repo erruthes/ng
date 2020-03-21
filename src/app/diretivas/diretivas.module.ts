e61bdb54e88e664cbdaa77e8bc22a8fef1260ea2
Eduardo Ruthes <eruthes@gmail.com>
2020-03-21 19:48:22 -0300

    estudo de HostListener e HostBinding

--- src/app/diretivas/diretivas.module.ts
+++ src/app/diretivas/diretivas.module.ts
@@ -10,6 +10,7 @@ import { OperadorElvisComponent } from './operador-elvis/operador-elvis.componen
 import { NgContentComponent } from './ng-content/ng-content.component';
 import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
 import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
+import { HighlightMouseDirective } from './shared/highlight-mouse.directive';
 
 
 
@@ -23,7 +24,8 @@ import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretiv
     OperadorElvisComponent,
     NgContentComponent,
     FundoAmareloDirective,
-    DiretivasCustomizadasComponent
+    DiretivasCustomizadasComponent,
+    HighlightMouseDirective
   ],
   imports: [
     CommonModule,
