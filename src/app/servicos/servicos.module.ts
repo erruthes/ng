5312d32b4c6df98887a9fa47b2e208fba52ea54c
Eduardo Ruthes <eruthes@gmail.com>
2020-03-22 18:00:18 -0300

    Refactoring do módulo de Serviços, injetando dependência

--- src/app/servicos/servicos.module.ts
+++ src/app/servicos/servicos.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CursosComponent } from './cursos/cursos.component';
-
-
+import { CursosService } from './cursos/cursos.service';
 
 @NgModule({
   declarations: [CursosComponent],
@@ -11,6 +10,9 @@ import { CursosComponent } from './cursos/cursos.component';
   ],
   exports: [
     CursosComponent
+  ],
+  providers: [
+    CursosService
   ]
 })
 export class ServicosModule { }
