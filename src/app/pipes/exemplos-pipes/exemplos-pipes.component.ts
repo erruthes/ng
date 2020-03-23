1210d0aa1899297a38e8151feff93f6005691a6e
Eduardo Ruthes <eruthes@gmail.com>
2020-03-23 07:59:48 -0300

    Pipes

---
+++ src/app/pipes/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+// import { registerLocaleData } from '@angular/common';
+// import localePt from '@angular/common/locales/pt';
+// registerLocaleData(localePt, 'pt');
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.css']
+})
+export class ExemplosPipesComponent implements OnInit {
+
+  book: any = {
+    title: 'Learning JavaScript Data Structure and Algorithms 2nd ed',
+    rating: 4.54321,
+    numPages: 314,
+    price: 44.99,
+    dateRelease: new Date(2016, 5, 23), // o indice di mes começa em 0
+    url: 'http://a.co/glqjpRP'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
