a91ba102e4656ddb6d527be4ca44cd688c0a94b5
Eduardo Ruthes <eruthes@gmail.com>
2020-03-31 08:05:58 -0300

    Refactoring do código

--- src/app/aap.routing.ts
+++ src/app/aap.routing.ts
@@ -1,11 +1,11 @@
-import { CursoNaoEncontradoComponent } from './rotas/curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursoNaoEncontradoComponent } from './rotas/cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CursosComponent } from './rotas/cursos/cursos.component';
 import { LoginComponent } from './rotas/login/login.component';
 import { HomeComponent } from './rotas/home/home.component';
-import { CursoDetalheComponent } from './rotas/curso-detalhe/curso-detalhe.component';
+import { CursoDetalheComponent } from './rotas/cursos/curso-detalhe/curso-detalhe.component';
 
 const APP_ROUTES: Routes = [
   { path: 'cursos', component: CursosComponent },
