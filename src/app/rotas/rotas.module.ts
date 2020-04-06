85139f6ffbd4998bc9f082fd505b15b34ed1eb2e
Eduardo Ruthes <eruthes@gmail.com>
2020-04-06 07:37:19 -0300

    Refactoring modularizando rotas/Cursos

--- src/app/rotas/rotas.module.ts
+++ src/app/rotas/rotas.module.ts
@@ -11,12 +11,13 @@ import { MatListModule } from '@angular/material/list';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
 import { RotasComponent } from './rotas.component';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
-import { CursosRoutingModule } from './cursos/cursos.routing.module';
+// import { CursosComponent } from './cursos/cursos.component';
+// import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+// import { CursosService } from './cursos/cursos.service';
+// import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+// import { CursosRoutingModule } from './cursos/cursos.routing.module';
+import { CursosModule } from './cursos/cursos.module';
 // import { AlunosModule } from './../alunos/alunos.module';
 // import { AlunosRoutingModule } from './../alunos/alunos.routing.module';
 import { RotasRoutingModule } from './rotas.routing.module';
@@ -27,16 +28,17 @@ import { RotasRoutingModule } from './rotas.routing.module';
     RotasComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    // CursosComponent,
+    // CursoDetalheComponent,
+    // CursoNaoEncontradoComponent
   ],
   imports: [
     CommonModule,
-    CursosRoutingModule,
+    // CursosRoutingModule,
     RotasRoutingModule,
     // AlunosModule,
     // AlunosRoutingModule,
+    CursosModule,
 
     MatMenuModule,
     MatIconModule,
@@ -48,7 +50,7 @@ import { RotasRoutingModule } from './rotas.routing.module';
     RotasComponent
   ],
   providers: [
-    CursosService
+    // CursosService
   ]
 })
 export class RotasModule { }
