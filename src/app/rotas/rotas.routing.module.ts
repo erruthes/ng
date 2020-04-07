7052b54349b14158f3923445d6fff0a5eadd698a
Eduardo Ruthes <eruthes@gmail.com>
2020-04-07 18:53:00 -0300

    Usando Guarda de Rotas: CanActivate

--- src/app/rotas/rotas.routing.module.ts
+++ src/app/rotas/rotas.routing.module.ts
@@ -4,15 +4,25 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './guards/auth.guard';
 // import { AlunosComponent } from './../alunos/alunos.component';
 
 const rotasRoutes: Routes = [
-  { path: 'cursos', loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule) },
+  { path: 'cursos',
+    loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule),
+    canActivate: [AuthGuard]
+  },
   // { path: 'alunos', component: AlunosComponent },
   // { path: 'alunos', loadChildren: 'app/alunos/alunos.module#AlunosModule' },
-  { path: 'alunos', loadChildren: () => import('./../alunos/alunos.module').then(m => m.AlunosModule) },
+  { path: 'alunos',
+    loadChildren: () => import('./../alunos/alunos.module').then(m => m.AlunosModule),
+    canActivate: [AuthGuard]
+  },
   { path: 'login', component: LoginComponent },
-  { path: '', component: HomeComponent }
+  { path: '',
+    component: HomeComponent,
+    canActivate: [AuthGuard]
+  }
 ];
 
 @NgModule({
