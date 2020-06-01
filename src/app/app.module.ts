d17441b532169a9e2b83a0901f02bdc0245262df
Eduardo Ruthes <eruthes@gmail.com>
2020-06-01 07:34:12 -0300

    Limpando declaração de módulos de Aulas anteriores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 registerLocaleData(localePt, 'pt');
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSliderModule } from '@angular/material/slider';
@@ -13,31 +13,31 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 // import { MatListModule } from '@angular/material/list';
 
 import { AppComponent } from './app.component';
-import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
-import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+// import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+// import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
-import { CursosModule } from './cursos/cursos.module';
-import { DataBindingModule } from './data-binding/data-binding.module';
-import { InputOutputPropertiesModule } from './input-output-properties/input-output-properties.module';
-import { LifeCicleModule } from './life-cicle/life-cicle.module';
-import { DiretivasModule } from './diretivas/diretivas.module';
+// import { CursosModule } from './cursos/cursos.module';
+// import { DataBindingModule } from './data-binding/data-binding.module';
+// import { InputOutputPropertiesModule } from './input-output-properties/input-output-properties.module';
+// import { LifeCicleModule } from './life-cicle/life-cicle.module';
+// import { DiretivasModule } from './diretivas/diretivas.module';
 import { SettingsService } from './shared/settings.service';
-import { PipesModule } from './pipes/pipes.module';
+// import { PipesModule } from './pipes/pipes.module';
 import { LogService } from './shared/log.service';
-import { ServicosModule } from './servicos/servicos.module';
-import { RotasModule } from './rotas/rotas.module';
+// import { ServicosModule } from './servicos/servicos.module';
+// import { RotasModule } from './rotas/rotas.module';
 import { AppRoutingModule } from './app.routing.module';
 // import { AlunosModule } from './alunos/alunos.module';
-import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
-import { FormulariosModule } from './formularios/formularios.module';
+// import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+// import { FormulariosModule } from './formularios/formularios.module';
 import { RequestHttpModule } from './request-http/request-http.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeuPrimeiroComponent,
-    MeuPrimeiro2Component,
-    PaginaNaoEncontradaComponent
+    // MeuPrimeiroComponent,
+    // MeuPrimeiro2Component,
+    // PaginaNaoEncontradaComponent
   ],
   imports: [
     BrowserModule,
@@ -48,16 +48,16 @@ import { RequestHttpModule } from './request-http/request-http.module';
     MatToolbarModule,
     // MatListModule,
 
-    CursosModule,
-    LifeCicleModule,
-    DataBindingModule,
-    DiretivasModule,
-    InputOutputPropertiesModule,
-    BrowserAnimationsModule,
-    ServicosModule,
-    PipesModule,
-    // RotasModule,
-    // routing
+    // CursosModule,
+    // LifeCicleModule,
+    // DataBindingModule,
+    // DiretivasModule,
+    // InputOutputPropertiesModule,
+    // BrowserAnimationsModule,
+    // ServicosModule,
+    // PipesModule,
+    // // RotasModule,
+    // // routing
     AppRoutingModule,
     // AlunosModule,
     // FormulariosModule,
