4fbfd4d68beeaf3490f0a39e171a62f4db42947b
Eduardo Ruthes <eruthes@gmail.com>
2020-06-23 08:02:25 -0300

    criando comportamento de Edição od curso

--- src/app/request-http/cursos/cursos-form/cursos-form.component.ts
+++ src/app/request-http/cursos/cursos-form/cursos-form.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { AlertModalService } from './../../shared/alert-modal.service';
 import { CursosService } from './../cursos.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
@@ -18,11 +19,34 @@ export class CursosFormComponent implements OnInit {
     private fb: FormBuilder,
     private service: CursosService,
     private modal: AlertModalService,
-    private location: Location) { }
+    private location: Location,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+
+    this.route.params.subscribe(
+      (params: any) => {
+        const id = params.id;
+        console.log(id);
+
+        const curso$ = this.service.loadById(id);
+        curso$.subscribe(curso => {
+          this.updateForm(curso);
+        });
+      }
+    );
+
     this.form = this.fb.group({
+      id: [null],
       nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(25)]]
+    });
+  }
+
+  updateForm(curso) {
+    this.form.patchValue({
+      id: curso.id,
+      nome: curso.nome
     });
   }
 
