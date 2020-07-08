69aa4de755a403f2708f7e238eb6ec2a5581d98e
Eduardo Ruthes <eruthes@gmail.com>
2020-07-08 07:43:04 -0300

    corrigir dependência do cursosService no Resolver

--- src/app/request-http/request-http.module.ts
+++ src/app/request-http/request-http.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ModalModule } from 'ngx-bootstrap/modal';
 
@@ -19,7 +20,8 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     // CursosModule,
     // UnsubscribeRxjsModule
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    HttpClientModule
   ],
   exports: [
     RequestHttpComponent
