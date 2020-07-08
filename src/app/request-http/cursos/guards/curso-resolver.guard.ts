83afee0fa2826a2dc82ac60b27ad500b9f896b81
Eduardo Ruthes <eruthes@gmail.com>
2020-07-08 07:47:51 -0300

    recuperar curso no Resolver

--- src/app/request-http/cursos/guards/curso-resolver.guard.ts
+++ src/app/request-http/cursos/guards/curso-resolver.guard.ts
@@ -16,11 +16,7 @@ export class CursoResolverGuard implements Resolve<Curso> {
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Curso> {
     if (route.params && route.params.id) {
-      return of({
-        id: null,
-        nome: null
-      });
-        // return this.service.loadById(route.params.id);
+        return this.service.loadById(route.params.id);
     }
 
     return of({
